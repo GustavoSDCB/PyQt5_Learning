695fa7208eb2353f30ef5c502ee7181c638714e4
Gustavo Silva de Carvalho Brito <gustavosdcb@hotmail.com>
2026-06-30 15:23:59 -0300

    Adding classes 10 and 11

--- Course Classes/messageBoxes.py
+++ Course Classes/messageBoxes.py
@@ -1,5 +1,8 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFont
+
+font = QFont("Times", 10, 100)     # Setting up a new Font Style
 
 class Window(QWidget):
     def __init__(self):
@@ -10,12 +13,44 @@ class Window(QWidget):
         self.setUI()
 
     def setUI(self):
+        # Warning Button
+        warningBtn = QPushButton("Warning", self)
+        warningBtn.setFont(font)    # Changing the Font Style
+        warningBtn.move(70, 150)
+        warningBtn.clicked.connect(self.warningAct)
+
+        # Info Button
+        infoBtn = QPushButton("Information", self)
+        infoBtn.setFont(font)
+        infoBtn.move(150, 150)
+        infoBtn.clicked.connect(self.infoAct)
+
         self.show()
+
+    def warningAct(self):
+        # Warning Message Box
+        mBox = QMessageBox.question(
+            self, 
+            "Warning", "Exit?", 
+            QMessageBox.Yes | QMessageBox.No, # You can add a third option addin pipe and Cancel i.g
+            QMessageBox.No
+            )
+        
+        sys.exit() if mBox == QMessageBox.Yes else None
+    
+    def infoAct(self):
+        # Information Message Box
+        mBox = QMessageBox.information(
+            self,
+            "Information",
+            "This is an Information Message Box",
+            QMessageBox.Ok
+        )
 
 def main():
     App = QApplication(sys.argv)
     window = Window()
-    sys.exit(App.exec_)
+    sys.exit(App.exec_())
 
 
 if __name__ == '__main__':
---
+++ Course Classes/spinBox.py
@@ -0,0 +1,50 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFont
+
+font = QFont("Comic", 15)
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(300, 300, 400, 300)
+        self.setWindowTitle("Spin Box")
+
+        self.setUI()
+
+    def setUI(self):
+        # SpinBox
+        self.spinBox = QSpinBox(self)
+        # Setting range
+        self.spinBox.setRange(1, 150)
+        # Setting Prefix/Sufix
+        self.spinBox.setPrefix("R$ ")
+        self.spinBox.setSuffix(",00")
+        # Setting the value of the step (when you click the arrow, how many values it'll increase/decrease each click)
+        self.spinBox.setSingleStep(2)
+        # Getting the value
+        value = self.spinBox.value()
+        # There's also a function of this object that you can call that is the same as the one in QPushButton (clicked.connect), here it is valueChanged.connect()
+        self.spinBox.move(100, 70)
+        self.spinBox.setFont(font)
+
+        # Submit
+        self.spinBtn = QPushButton("Submit Value", self)
+        self.spinBtn.setFont(QFont("Times", 10))
+        self.spinBtn.move(100, 100)
+        self.spinBtn.clicked.connect(self.getSpinValue)
+
+        self.show()
+
+    def getSpinValue(self):
+        spinValue = self.spinBox.value()
+        print(f"You choose R${spinValue:.2f}")
+
+def main():
+    App = QApplication(sys.argv)
+    window = Window()
+    sys.exit(App.exec_())
+
+
+if __name__ == '__main__':
+    main()
