2ddd72b160bb0f9ed052bf165e66555f89f5f803
Gustavo Silva de Carvalho Brito <gustavosdcb@hotmail.com>
2026-07-07 12:00:45 -0300

    Refactoring the code to be more clear and applying new concepts

--- Course Classes/Section 2/rockPaperScisor.py
+++ Course Classes/Section 2/rockPaperScisor.py
@@ -19,6 +19,18 @@ paths = {
     'paper': r"Course Classes\images\paper.jpg"
 }
 
+positionPlayers = {
+    'player': 135,
+    'computer': 225
+}
+
+positionButtons = {
+    'scisor': 100,
+    'rock': 170,
+    'paper': 240
+}
+
+
 class Window(QWidget):
     def __init__(self):
         super().__init__()
@@ -34,91 +46,39 @@ class Window(QWidget):
         self.title.setFont(font)
         self.title.move(60, 1)
 
-        ######### Game Images Setup #########
-        self.gameSetUp()
+        # Game Images Setup
+        self.setGameImages()
 
-        # Scisor Button
-        self.scisorBtn = QPushButton(self)
-        self.scisorBtn.setIcon(QIcon(paths['scisor']))
-        self.scisorBtn.setIconSize(QSize(50, 50))
-        self.scisorBtn.setFlat(True)
-        self.scisorBtn.setCursor(QCursor(Qt.PointingHandCursor))
-        self.scisorBtn.move(100, 200)
-        self.scisorBtn.clicked.connect(lambda: self.game('scisor'))
-
-        # Rock Button
-        self.rockBtn = QPushButton(self)
-        self.rockBtn.setIcon(QIcon(paths['rock']))
-        self.rockBtn.setIconSize(QSize(50, 50))
-        self.rockBtn.setFlat(True)
-        self.rockBtn.setCursor(QCursor(Qt.PointingHandCursor))
-        self.rockBtn.move(170, 200)
-        self.rockBtn.clicked.connect(lambda: self.game('rock'))
-
-        # Paper Button
-        self.paperBtn = QPushButton(self)
-        self.paperBtn.setIcon(QIcon(paths['paper']))
-        self.paperBtn.setIconSize(QSize(50, 50))
-        self.paperBtn.setCursor(QCursor(Qt.PointingHandCursor))
-        self.paperBtn.setFlat(True)
-        self.paperBtn.move(240, 200)
-        self.paperBtn.clicked.connect(lambda: self.game('paper'))
+        # Game Buttons Setup
+        self.setGameButtons()
 
         self.show()
 
-    def gameSetUp(self):
-        # positions = {
-        #     'player': 135,
-        #     'computer': 185
-        # }
+    def setGameImages(self):
+        self.images = {}
 
-        # for player, x in positions.items():
-        #     pass
+        for player, x in positionPlayers.items():
+            for choice, path in paths.items():
+                label = QLabel(self)
+                label.setPixmap(QPixmap(path))
+                label.resize(50, 50)
+                label.setScaledContents(True)
+                label.move(x, 110)
+                # label.hide()
+                self.images[(player, choice)] = label
 
+    def setGameButtons(self):
+        self.buttons = {}
 
-        # Player (scisor, rock, paper)
-        self.pScisorImg = QLabel(self)
-        self.pScisorImg.setPixmap(QPixmap(paths['scisor']))
-        self.pScisorImg.resize(50, 50)
-        self.pScisorImg.setScaledContents(True)
-        self.pScisorImg.move(135, 110)
-        # self.pScisorImg.hide()
-
-        self.pRockImg = QLabel(self)
-        self.pRockImg.setPixmap(QPixmap(paths['rock']))
-        self.pRockImg.resize(50, 50)
-        self.pRockImg.setScaledContents(True)
-        self.pRockImg.move(135, 110)
-        self.pRockImg.hide()
-
-        self.pPaperImg = QLabel(self)
-        self.pPaperImg.setPixmap(QPixmap(paths['paper']))
-        self.pPaperImg.resize(50, 50)
-        self.pPaperImg.setScaledContents(True)
-        self.pPaperImg.move(135, 110)
-        self.pPaperImg.hide()
-
-        # Computer (scisor, rock, paper)
-        self.cScisorImg = QLabel(self)
-        self.cScisorImg.setPixmap(QPixmap(paths['scisor']))
-        self.cScisorImg.resize(50, 50)
-        self.cScisorImg.setScaledContents(True)
-        self.cScisorImg.move(185, 110)
-        self.cScisorImg.hide()
-
-        self.cRockImg = QLabel(self)
-        self.cRockImg.setPixmap(QPixmap(paths['rock']))
-        self.cRockImg.resize(50, 50)
-        self.cRockImg.setScaledContents(True)
-        self.cRockImg.move(185, 110)
-        self.cRockImg.hide()
-
-        self.cPaperImg = QLabel(self)
-        self.cPaperImg.setPixmap(QPixmap(paths['paper']))
-        self.cPaperImg.resize(50, 50)
-        self.cPaperImg.setScaledContents(True)
-        self.cPaperImg.move(185, 110)
-        self.cPaperImg.hide()
+        for choice, path in paths.items():
+            label = QPushButton(self)
+            label.setIcon(QIcon(path))
+            label.setIconSize(QSize(50, 50))
+            label.setFlat(True)
+            label.setCursor(QCursor(Qt.PointingHandCursor))
+            label.move(positionButtons[choice], 200)
+            label.clicked.connect(lambda checked, c=choice: self.game(c))
+            self.buttons[choice] = label
 
     def game(self, player:str):
         # Computer's Hand
